487d3cda25865c5cec5c0456e90862828b6f8f1d
Артем <marcus201@mail.ru>
2022-09-09 00:14:40 +0500

    Исправлено

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,11 @@ public class Main {
         part3task2();
     }
 
+
+
+
     private static void part3task2() {
+        System.out.println("Задание 9");
         int salary = 29000;
         int totalSalary = 0;
         for (int i = 1; i <= 12; i ++){
@@ -23,6 +27,7 @@ public class Main {
     }
 
     private static void part3task1() {
+        System.out.println("Задание 8");
         int salary = 29000;
         int totalSalary = 0;
         for(int i = 1; i <= 12; i++){
@@ -32,37 +37,45 @@ public class Main {
     }
 
     private static void part2task3() {
-        for(int i = 1; i >= 1 && i <=512; i = i *2){
-            System.out.println(+ i);
+        System.out.println("Задание 7");
+        for (int i = 1; i >= 1 && i <= 512; i = i * 2) {
+            System.out.println(i + "");
         }
     }
 
     private static void part2task2() {
-        for(int i = 7; i >= 7 && i <= 98; i = i + 7){
-            System.out.println(+ i);
+        System.out.println("Задание 6");
+        for(int i = 7; i >= 7 && i <= 98; i = i + 7) {
+            System.out.println(i + " ");
         }
     }
 
     private static void part2task1() {
-        for (int i = 1904; i%4 == 0 && i <= 2096; i = i + 4){
-            System.out.println(+i + " год является високосным");
+        System.out.println("Задание 5");
+        for (int i = 1904; i <= 2096; i++) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+                System.out.println(i + " год является високосным");
+            }
         }
     }
 
 
     private static void part1task4() {
+        System.out.println("Задание 4");
         for(int i = 10; i <=10 && i >= -10; i --){
             System.out.println(+ i);
         }
     }
 
     private static void part1task3() {
+        System.out.println("Задание 3");
         for (int i = 2; i % 2 == 0 && i < 17; i ++){
             System.out.println(+ i ++);
         }
     }
 
     private static void part1task2() {
+        System.out.println("Задание 2");
         for (int i = 10; i >= 1; i --){
             System.out.println(+ i);
         }
@@ -70,6 +83,7 @@ public class Main {
     }
 
     private static void part1task1() {
+        System.out.println("Задание 1");
         for (int i = 1; i <= 10; i++) {
             System.out.println(+ i);
         }
